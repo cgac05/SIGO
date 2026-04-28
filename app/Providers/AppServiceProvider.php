bbad77542a9b7364957a489feb077ca51b05f2de
Christian Guillermo Avila Camberos <chguavilaca@ittepic.edu.mx>
2026-04-28 02:04:35 -0700

    Fix: Desactivar forceScheme https para evitar ERR_CONNECTION_TIMED_OUT en Elastic Beanstalk directo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        /* 
         if (config('app.env') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+        */
 
         // Registrar observers para modelos
         HitosApoyo::observe(HitosApoyoObserver::class);
